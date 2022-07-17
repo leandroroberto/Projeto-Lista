8a8da7dac4fe3c4c046ae70d233702c520e1a1ea
leandroroberto <70127308+leandroroberto@users.noreply.github.com>
2022-07-17 01:14:01 -0300

    Add files via upload
    Foi mudado algumas configurações relacionadas ao CSS
    Adicionado a opção de marcar os itens que já foram obtidos da lista
    Melhorado o botão "Limpar Lista"

--- ProjetoLista/script/script.js
+++ ProjetoLista/script/script.js
@@ -1,27 +1,55 @@
-
-function removerItem (button) {
+function removerItem(button) {
     let remove = button.parentNode
     document.getElementById("lista-produtos").removeChild(remove)
 
 }
 
-document.addEventListener("keypress", function(e){
-    if (e.which == 13){
-        adicionarProduto()
+function limparLista() {
+    document.getElementById("lista-produtos").innerHTML = ""
+    document.getElementById("lista-produtos").innerText = "...Lista vazia"
+}
+
+
+function limparUl() {
+    if (document.getElementById("lista-produtos").innerText == "...LISTA VAZIA") {
+        document.getElementById("lista-produtos").innerText = ""
     }
-}, false)
+}
 
 
-function adicionarProduto(){
+function adicionarProduto() {
+
+    // Chamar função de limpar
+
+    limparUl()
+    
+    // Cria o objeto e personaliza
+
     let produto = document.querySelector("input[name='produto']").value
     let novaLista = document.createElement("li")
+    novaLista.setAttribute("id", "li-lista")
     novaLista.style.margin = "7px"
+    novaLista.style.cursor = "pointer"
+    novaLista.addEventListener("click", () => {
+        if(novaLista.style.textDecoration == "line-through"){
+            novaLista.style.textDecoration = ""
+        } else {
+            novaLista.style.textDecoration = "line-through"
+        }
+    })
+    novaLista.style.fontSize = "1rem"
     novaLista.innerText = produto
+
+    // Verifica se o input de produto está vazio para continuar adicionando
+
     if (produto) {
         document.getElementById("lista-produtos").appendChild(novaLista)
         document.querySelector("input[name='produto']").value = ""
     }
-    
+
+
+    // Botão Remover
+
     let removeButton = document.createElement("button")
     removeButton.setAttribute("onclick", "removerItem(this)")
     removeButton.setAttribute("id", "remove-item")
@@ -37,6 +65,20 @@ function adicionarProduto(){
     novaLista.appendChild(removeButton)
 
 
+    // Adiciona a tecla "Enter" como envio do produto para lista
+
+    document.addEventListener("keypress", function (e) {
+        if (e.which == 13) {
+            adicionarProduto()
+        }
+    }, false)
+
+
+
     window.event.preventDefault()
+
+    
 }
+
+
 
